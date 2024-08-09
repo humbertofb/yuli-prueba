4050f9aa449092757ced89dbe65b52bfd9512fbc
humbertofb <98710153+humbertofb@users.noreply.github.com>
2024-08-09 12:19:39 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
+document.getElementById('menuIcon').addEventListener('click', function() {
+    const menu = document.getElementById('menu');
+    if (menu.style.display === 'none' || menu.style.display === '') {
+        menu.style.display = 'block';
+    } else {
+        menu.style.display = 'none';
+    }
+});
+
 document.getElementById('surpriseBtn').addEventListener('click', function() {
     alert('¡Te quiero muchísimo, no olvides lo especial que eres para mí!');
 });
