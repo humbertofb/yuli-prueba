81815ec3151ded1a42f11651ec3c959ad1e6ff29
humbertofb <98710153+humbertofb@users.noreply.github.com>
2024-08-20 05:50:52 +0200

    Actualizar firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+// Importa las funciones que necesitas de Firebase SDK
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Tu configuración de Firebase
+const firebaseConfig = {
+    apiKey: "TU_API_KEY",
+    authDomain: "TU_AUTH_DOMAIN",
+    projectId: "TU_PROJECT_ID",
+    storageBucket: "TU_STORAGE_BUCKET",
+    messagingSenderId: "TU_MESSAGING_SENDER_ID",
+    appId: "TU_APP_ID"
+};
+
+// Inicializa Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
